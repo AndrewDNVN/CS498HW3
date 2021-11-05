9bfe3272b491f0475477fae7b6ea66c2e4bd0133
AndrewDnvn <andrewdnvn8@gmail.com>
2021-11-05 11:12:59 -0400

    fin

--- test_reverse.cpp
+++ test_reverse.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <cstring>
 
-
+//Given
 std::string reversed(const std::string &in)
 {
     std::string out{};
@@ -15,10 +15,68 @@ std::string reversed(const std::string &in)
     return out;
 }
 
-TEST_CASE( "Simple tests 1 works 2 does not:" ) {
+// uncomment for submission
+//std::string reversed(const std::string &in);
+
+TEST_CASE( "Single char:" "[Single]" ) {
+    REQUIRE( reversed("a") == "a");
+
+    REQUIRE( reversed("A") == "A");
+
+    REQUIRE( reversed(" ") == " "); // spaces should be unaffected
+}
+
+TEST_CASE( "Simple tests:" "[Small]" ) {
     REQUIRE( reversed("abc") == "cba");
 
-    //should fail
-    REQUIRE( reversed("abc") == "abc");
+    REQUIRE( reversed("123") == "321");
 }
+
+TEST_CASE( "Simple failing tests:" "[Fail]" ) {
+    REQUIRE( reversed("abc") == "abc");
+
+    // Another point of intrest with catch2
+    // is when in a test case when one Require 
+    // fails it just moves onto the next block.
+    // Which is strange to me; I feel it should test 
+    // no matter if one has failed.
+    // Or at least acknowledge that the 
+    // other states in the block.
+
+    REQUIRE( reversed("123") == "123");
+}
+
+
+TEST_CASE( "Even number tests:" ) {
+    REQUIRE( reversed("abcd") == "dcba");
+
+
+    REQUIRE( reversed("1234") == "4321");
+}
+
+
+TEST_CASE( "Same word [Palindrome]:" ) {
+    REQUIRE( reversed("bob") == "bob");
+
+    //should fail
+    REQUIRE( reversed("racecar") == "racecar");
+}
+
+
+// Suspected Bouns testing fail case:
+// passing a null char.
+
+TEST_CASE( "Bouns:" "[bouns]" ) {
+
+    std::string in = "ab\0cd";
+    std::string out = "dc\0ba";
+
+    REQUIRE( reversed(in) == out);
+}
+
+
+//    The above should fail becuase it is a hold over from
+//    C where that '\0' marked the end of the char array.
+//    So in this case it stops the string from reading beyond.
+//    That is why it fails the Test case.
 
